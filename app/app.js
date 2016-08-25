37f733bad156379834567410b1c17c394647bc22
yuyan <yuyang545262477@gmail.com>
2016-08-25 21:50:55 +0800

    调整部分文件

--- app/app.js
+++ app/app.js
@@ -1,14 +1,6 @@
-'use strict';
+(function () {
+    "use strict";
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    angular.module('ionic', []);
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+})();
